ace55fbd07094ddc844514b3232bfe9423defff9
laecio.sousa.barboza <laecio.sousa.barboza@accenture.com>
2022-02-26 19:13:03 -0300

    Init

---
+++ personagens/src/test/java/com/udemy/personagens/PersonagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.personagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
